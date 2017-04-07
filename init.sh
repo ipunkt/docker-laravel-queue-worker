b0c20c1cba8eff06b48a382323e4a8cd7dd2d698
Robert Kummer <rok@ipunkt.biz>
2017-04-07 14:23:26 +0200

    do not remove tpl file

--- init.sh
+++ init.sh
@@ -12,6 +12,6 @@ sed -e "s~%%CONNECTION%%~$CONNECTION~" \
 	-e "s~%%QUEUE%%~$QUEUE~" \
 	/etc/supervisor/conf.d/laravel-worker.conf.tpl > /etc/supervisor/supervisord.conf
 
-rm /etc/supervisor/conf.d/laravel-worker.conf.tpl
+#rm /etc/supervisor/conf.d/laravel-worker.conf.tpl
 
 supervisord --nodaemon --configuration /etc/supervisor/supervisord.conf
