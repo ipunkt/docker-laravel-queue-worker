35f319c81d3aa046d78834c3060e5400da08cd67
Robert Kummer <github@mail.robert-kummer.de>
2017-11-22 11:29:04 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,8 +8,20 @@ if [ -z "$QUEUE_NAME" ]; then
 	QUEUE_NAME="default"
 fi
 
-sed -e "s~%%QUEUE_CONNECTION%%~$QUEUE_CONNECTION~" \
-	-e "s~%%QUEUE_NAME%%~$QUEUE_NAME~" \
-	/etc/supervisor/conf.d/laravel-worker.conf.tpl > /etc/supervisor/supervisord.conf
+if [ -z "$LARAVEL_HORIZON" ]; then
+	LARAVEL_HORIZON=false
+fi
+
+if [ "$LARAVEL_HORIZON" = false ]; then
+	sed -e "s~%%QUEUE_CONNECTION%%~$QUEUE_CONNECTION~" \
+		-e "s~%%QUEUE_NAME%%~$QUEUE_NAME~" \
+		/etc/supervisor/conf.d/laravel-worker.conf.tpl > /etc/supervisor/supervisord.conf
+fi
+
+if [ "$LARAVEL_HORIZON" = true ]; then
+	sed -e "s~%%QUEUE_CONNECTION%%~$QUEUE_CONNECTION~" \
+		-e "s~%%QUEUE_NAME%%~$QUEUE_NAME~" \
+		/etc/supervisor/conf.d/laravel-horizon.conf.tpl > /etc/supervisor/supervisord.conf
+fi
 
 supervisord --nodaemon --configuration /etc/supervisor/supervisord.conf
