288ccf973ad36dcd6fd8917a86159f2ff2306d11
rok <rok@ipunkt.biz>
2019-05-23 11:11:58 +0200

    supporting multiple truethy values for LARAVEL_HORIZON environment variable

--- init.sh
+++ init.sh
@@ -16,15 +16,13 @@ PHP_MEMORY_LIMIT=${PHP_MEMORY_LIMIT:-512}
 sed -e "s~%%MEMORY_LIMIT%%~${PHP_MEMORY_LIMIT}m~" \
     /opt/etc/custom-php.ini.tpl > /usr/local/etc/php/conf.d/custom-php.ini
 
-if [ "$LARAVEL_HORIZON" = false ]; then
+if [ "$LARAVEL_HORIZON" = "true" ] || [ "$LARAVEL_HORIZON" = "1" ] ; then
+	cp /etc/supervisor/conf.d/laravel-horizon.conf.tpl /etc/supervisor/supervisord.conf
+else
 	sed -e "s~%%QUEUE_CONNECTION%%~$QUEUE_CONNECTION~" \
 		-e "s~%%QUEUE_NAME%%~$QUEUE_NAME~" \
 		-e "s~%%MEMORY_LIMIT%%~$PHP_MEMORY_LIMIT~" \
 		/etc/supervisor/conf.d/laravel-worker.conf.tpl > /etc/supervisor/supervisord.conf
-fi
-
-if [ "$LARAVEL_HORIZON" = true ]; then
-	cp /etc/supervisor/conf.d/laravel-horizon.conf.tpl /etc/supervisor/supervisord.conf
 fi
 
 exec supervisord --nodaemon --configuration /etc/supervisor/supervisord.conf
